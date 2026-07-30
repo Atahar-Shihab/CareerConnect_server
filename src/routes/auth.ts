c9431e7a9abab483946c4425829b36cbd66f6f6e
Atahar Shihab <shihabatahar@gmail.com>
2026-07-30 16:38:31 +0600

    feat(routes): configure Auth REST API routes (/api/auth)

---
+++ src/routes/auth.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { register, login, getMe, googleLogin } from '../controllers/auth';
+import { authenticate } from '../middleware/auth';
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/google', googleLogin);
+router.get('/me', authenticate, getMe);
+
+export default router;
